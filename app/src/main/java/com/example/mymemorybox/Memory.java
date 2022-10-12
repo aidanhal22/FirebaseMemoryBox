8634a429dec3e1cde3c144bf2bfb5b5439585a62
aidanhal22 <90794198+aidanhal22@users.noreply.github.com>
2022-10-12 11:04:07 -0500

    10-12 FINISHED PARCELABLE

--- app/src/main/java/com/example/mymemorybox/Memory.java
+++ app/src/main/java/com/example/mymemorybox/Memory.java
@@ -1,6 +1,9 @@
 package com.example.mymemorybox;
 
-public class Memory {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Memory implements Parcelable{
     private int rating, imageResourceId;
     private String name, desc;
 
@@ -23,6 +26,54 @@ public class Memory {
         this.imageResourceId = 0;
         this.name = "";
         this.desc = "";
+    }
+    public static final Parcelable.Creator<Memory> CREATOR = new
+            Parcelable.Creator<Memory>() {
+
+                @Override
+                public Memory createFromParcel(Parcel parcel) {
+                    return new Memory(parcel);
+                }
+
+                @Override
+                public Memory[] newArray(int size) {
+                    return new Memory[0];
+                }
+            };
+    /** This is a "constructor" of sorts that is needed with the Parceable interface to
+     * tell the intent how to create a Food object when it is received from the intent
+     * basically it is setting each instance variable as a String or Int
+     * if the instance variables were objects themselves you would need to do more complex * code.  We need to read in the String, double, and int data.
+     *
+     * @param parcel    the parcel that is received from the intent
+     */
+    // to avoid errors keep the order of the parameters that you are setting the same in all
+    // of these methods it should match what you have in your original constructor
+    public Memory(Parcel parcel) {
+        rating = parcel.readInt();
+        imageResourceId = parcel.readInt();
+        name = parcel.readString();
+        desc = parcel.readString();
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    /**
+     * This is what is used when we send the Food object through an intent
+     * It is also a method that is part of the Parceable interface and is needed
+     * to set up the object that is being sent.  Then, when it is received, the
+     * other Food constructor that accepts a Parcel reference can "unpack it"
+     *
+     */
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeInt(rating);
+        dest.writeInt(imageResourceId);
+        dest.writeString(name);
+        dest.writeString(desc);
     }
 
     public int getRating() {
